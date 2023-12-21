6f6a06c34e042630653a205c075ab45227f12374
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2023-12-21 14:58:47 +0700

    shadow DOM

--- src/script/components/club-list.js
+++ src/script/components/club-list.js
@@ -1,5 +1,9 @@
 import './club-item.js';
 class ClubList extends HTMLElement {
+    constructor(){
+        super();
+        this.shadowDOM = this.attachShadow({mode: 'open'});
+    }
     set clubs(clubs) {
         this._clubs = clubs;
         this.render();
@@ -10,13 +14,23 @@ class ClubList extends HTMLElement {
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement('club-item');
             clubItemElement.club = club;
-            this.appendChild(clubItemElement);
+            this.shadowDOM.appendChild(clubItemElement);
         });
     }
 
     renderError(message) {
-        this.innerHTML = '';
-        this.innerHTML += `<h2 class ="placeholder">${message}</h2>`;
+        this.shadowDOM.innerHTML = '';
+        this.shadowDOM.innerHTML += `
+          .placeholder {
+            font-weight: lighter;
+            color: rgba(0, 0, 0, 0.5);
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+          }
+         
+        <h2 class ="placeholder">${message}</h2>`;
     }
 }
 
