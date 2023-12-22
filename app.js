37ddf4c15c559eb90a0b677d3b9d307b7461bb87
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2023-12-22 15:44:26 +0700

    menambahkan webpack

--- app.js
+++
@@ -1,7 +0,0 @@
-import main  from "./src/script/view/main.js";
-import './src/script/components/app-bar.js'
-
-// const appBarElement = document.createElement('app-bar');
-// document.body.appendChild(appBarElement);
-
-document.addEventListener('DOMContentLoaded', main);
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime';
+import main  from "./script/view/main.js";
+import './script/components/app-bar.js';
+import './styles/style.css';
+
+// const appBarElement = document.createElement('app-bar');
+// document.body.appendChild(appBarElement);
+
+document.addEventListener('DOMContentLoaded', main);
